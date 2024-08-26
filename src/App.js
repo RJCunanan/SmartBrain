8019200e00243bec91ebcac553ef032f58b345ec
RJ Cunanan <rjlugtuc@gmail.com>
2024-08-25 18:05:35 -0700

    Added face recognition feature to app using Clarifai API.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,121 @@
+import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ParticlesBg from 'particles-bg';
+// import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg className='particles' color="#FFFFFF" num={200} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*{
-      <FaceRecognition />} */}
-    </div>
-  );
+// You must add your own API key here from Clarifai.
+// const app = new Clarifai.App({
+//   apiKey: '303aa36d46ae4e2fa8c3de8552aeb8f5'
+// });
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = '6a1673436d4a4674967c428826de7f78';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'nocturtle';       
+  const APP_ID = 'smartbrain';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
+
+const getModelId = () => {
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  return MODEL_ID;
+}
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'home'
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    /*
+    app.models.predict('face-detection', this.state.input)
+    */
+    fetch("https://api.clarifai.com/v2/models/" + getModelId() + "/outputs", returnClarifaiRequestOptions(this.state.input))
+      .then(response => response.json())
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg className='particles' color="#FFFFFF" num={200} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
